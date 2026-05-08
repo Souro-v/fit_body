2d1718026c3d6c8185032dafc6e0431430ff7b01
sourov <sourov.kpi21@gmail.com>
2026-05-08 23:29:26 +0600

    assets update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'routes/app_routes.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+
+  await Firebase.initializeApp();
+
   // Full-screen immersive experience
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
@@ -12,9 +15,9 @@ Future<void> main() async {
       statusBarIconBrightness: Brightness.light,
     ),
   );
+
   runApp(const FitBodyApp());
 }
-
 class FitBodyApp extends StatelessWidget {
   const FitBodyApp({super.key});
 
